7c54e5e412e2b0a3db97286eb0833a530d50308c
M3tatr0N_ <109506437+0x-raafet@users.noreply.github.com>
2024-01-05 13:52:35 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/0x-raafet/openbullet_setup/main/ng.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
@@ -31,3 +31,15 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo create root password
 passwd
 echo "===================================="
+echo "Installing Dotnet ..."
+wget https://download.visualstudio.microsoft.com/download/pr/1cac4d08-3025-4c00-972d-5c7ea446d1d7/a83bc5cbedf8b90495802ccfedaeb2e6/dotnet-sdk-6.0.417-linux-x64.tar.gz > /dev/null 2>&1
+mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-6.0.417-linux-x64.tar.gz -C $HOME/dotnet > /dev/null 2>&1
+export DOTNET_ROOT=$HOME/dotnet
+export PATH=$PATH:$HOME/dotnet
+git clone https://github.com/openbullet/OpenBullet2/  > /dev/null 2>&1
+cd OpenBullet2/OpenBullet2
+dotnet publish --configuration Release  > /dev/null 2>&1
+cd bin/Release/net6.0/publish
+nohup dotnet ./OpenBullet2.dll &>/dev/null &
+nohup ./ngrok http 5000 &>/dev/null &
+curl --silent --show-error http://127.0.0.1:4041/api/tunnels | sed -nE 's/.*public_url":"http:..([^"]*).*/\1/p'
